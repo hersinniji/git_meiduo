f9e72419a3c36091cf114456f51b534eb1ee9263
hersin <1508625083@qq.com>
2019-06-11 23:49:42 +0800

    用户模型编写

--- meiduo_hersin/apps/users/models.py
+++ meiduo_hersin/apps/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+1. 自己定义模型
+    class User(models.Model):
+        username = 
+        password = 
+        mobile = 
+    密码是明文,我们自己要完成验证,等等问题
+2. 我们发现我们在学习基础的时候,django自带了admin后台管理
+    admin 后台管理,也有用户信息的保存和认证,密码是密文,也可以验证用户信息
+    所以本次使用django自带的用户模型
+"""
+# 当系统的类/方法不能满足我们需求的时候,我们就继承/重写
+# from django.contrib.auth.models import AbstractUser
+#
+#
+# class User(AbstractUser):
+#     moble = models.CharField(max_length=11, unique=True, verbose_name='手机号')
--- meiduo_hersin/apps/users/views.py
+++ meiduo_hersin/apps/users/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.views import View
+# Create your views here.
 
 
 class RegisterView(View):
 
+    """
+    1.用户名需要分析是否重复(这里需要一个视图)
+        用户名的长度有5-20个的要求
+    2.密码 有长度的限制 8-20个,要求为数字,字母,_
+    3.确认密码 和密码一致
+    4.手机号 手机号得先满足规则
+        再判断手机号是否重复
+    5.图片验证码是一个后端功能
+        图片验证码是为了防止 计算开攻击我们发送短信的功能
+    6.短信发送
+    7.必须同意协议
+    8.注册也是一个功能
+
+    必须要和后端交互的是:
+    1.用户名/手机号是否重复
+    2.图片验证码
+    3.短信
+    4.注册功能
+    """
+
     def get(self, request):
         return render(request, 'register.html')
+
+    def post(self, request):
+        """
+        1.接收前端提交的用户名,密码和手机号
+        2.入库
+        3.响应
+        :param request:
+        :return:
+        """
