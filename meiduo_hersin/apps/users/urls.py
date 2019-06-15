9753b373e4464edaf7ba6343f158556b2fc9e830
hersin <1508625083@qq.com>
2019-06-15 16:55:33 +0800

    登录界面的前端展示

--- meiduo_hersin/apps/users/urls.py
+++ meiduo_hersin/apps/users/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # 这里可以给匹配的路由起个名字,见名知意
     url(r'^register/', views.RegisterView.as_view(), name='register'),
     url(r'^usernames/(?P<username>[0-9a-zA-Z_]{5,20})/count/$', views.UsernameCountView.as_view(), name='usernamecount'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
 
 ]
