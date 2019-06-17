7e78886c23b96ab28b1ab0ac04afc9ad8955dddf
hersin <1508625083@qq.com>
2019-06-17 17:57:02 +0800

    实现qq跳转的url（使用QQLoginTool）

--- meiduo_hersin/apps/oauth/views.py
+++ meiduo_hersin/apps/oauth/views.py
@@ -1,13 +1,17 @@
+from QQLoginTool.QQtool import OAuthQQ
 from django import http
 from django.shortcuts import render
 
 # Create your views here
 from django.views import View
 
+from meiduo_hersin import settings
+
 """
 1.实现登录界面 QQ 按钮的跳转
-    ① 直接把拼接号的url放在a标签上面 https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=101518219&redirect_uri=http://www.meiduo.site:8000/oauth_callback&state=test
-    ② 可以通过让前端发送一个ajax请求
+    方法① 直接把拼接号的url放在a标签上面 https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=101518219&redirect_uri=http://www.meiduo.site:8000/oauth_callback&state=test
+    方法② 可以通过让前端发送一个ajax请求
+    方法③ 使用QQ登录工具 QQLoginTool，可以通过输入参数自行拼接url
 """
 
 
@@ -15,4 +19,20 @@ class QQAuthUserView(View):
 
     def get(self, request):
 
-        return http.JsonResponse({'login_url': 'https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=101518219&redirect_uri=http://www.meiduo.site:8000/oauth_callback&state=test'})
+        # 方法②
+        # return http.JsonResponse({'login_url': 'https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=101518219&redirect_uri=http://www.meiduo.site:8000/oauth_callback&state=test'})
+
+        # 方法③
+        # 1.创建实例对象
+        state = 'test'
+        qqoauth = OAuthQQ(
+            client_id=settings.QQ_CLIENT_ID,
+            client_secret=settings.QQ_CLIENT_SECRET,
+            redirect_uri=settings.QQ_REDIRECT_URI,
+            state=state
+        )
+
+        # 2.调用方法
+        login_url = qqoauth.get_qq_url()
+
+        return http.JsonResponse({'login_url': login_url})
