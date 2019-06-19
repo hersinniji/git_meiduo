cbeffb5481546361c1e2396ccfd227291a677b5d
hersin <1508625083@qq.com>
2019-06-19 13:03:54 +0800

    邮件的celery异步发送(含邮件/短信retry发送重试)

--- meiduo_hersin/celery_tasks/email/tasks.py
+++ meiduo_hersin/celery_tasks/email/tasks.py
@@ -6,8 +6,8 @@ from celery_tasks.main import app
 from meiduo_hersin import settings
 
 
-@app.task
-def send_active_email(email):
+@app.task(bind=True, default_time_delay=5)
+def send_active_email(self, email):
     # 4.发送激活邮件
     from django.core.mail import send_mail
 
@@ -21,13 +21,16 @@ def send_active_email(email):
     message = '内容'
     from_email = settings.EMAIL_FROM
     recipient_list = [email]
-    html_message = '<h1>你好,王佳星!</h1><br><h2>欢迎来到美多商城!</h2><br>'
+    html_message = '<h1>你好,王佳星!</h1><br><h2>欢迎来到美多商城!</h2><br><h2>请开始你的表演!</h2><br>'
 
     # 能够发送邮件给用户的邮箱
-    send_mail(
-        subject=subject,
-        message=message,
-        from_email=from_email,
-        recipient_list=recipient_list,
-        html_message=html_message
-    )
+    try:
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=from_email,
+            recipient_list=recipient_list,
+            html_message=html_message
+        )
+    except Exception as e:
+        raise self.retry(exc=e, max_retries=3)
--- meiduo_hersin/celery_tasks/sms/tasks.py
+++ meiduo_hersin/celery_tasks/sms/tasks.py
@@ -6,7 +6,34 @@ from libs.yuntongxun.sms import CCP
 from celery_tasks.main import app
 
 
-@app.task
-def send_sms_code(mobile, sms_code):
+"""
+bind = True
+第一参数是将始终是任务实例(self)
+default_retry_delay 默认每次重试间隔的时间
+"""
 
-    CCP().send_template_sms(mobile, [sms_code, 5], 1)
+
+@app.task(bind=True,default_retry_delay=5)
+def send_sms_code(self,mobile,sms_code):
+
+    try:
+        result = CCP().send_template_sms(mobile, [sms_code, 5], 1)
+    except Exception as e:
+        raise self.retry(exc=e,max_retries=10)
+
+    if result != 0:
+        # 不是0 都是失败 ,失败就应该重试
+        raise self.retry(exc=Exception('baby,just check your configuration parameter!!!'),max_retries=3)
+
+
+# @app.task(bind=True, default_retry_delay=5)
+# def send_sms_code(self, mobile, sms_code):
+#
+#     try:
+#         result = CCP().send_template_sms(mobile, [sms_code, 5], 1)
+#     except Exception as e:
+#         raise self.retry(exc=e, max_retries=10)
+#     if result != 0:
+#         # 不是0都是失败,失败就应该重试
+#         # 这里最大重试次数设定为10次
+#         raise self.retry(exc=Exception('baby,just check your configuration parameter!!!'), max_retries=3)
