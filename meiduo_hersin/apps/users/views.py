6ac23d9e8c1dae41b55fbfbc8fe163bc6c162136
hersin <1508625083@qq.com>
2019-06-21 16:01:19 +0800

    定义首页广告模型类和商品信息模型类

--- meiduo_hersin/apps/users/views.py
+++ meiduo_hersin/apps/users/views.py
@@ -433,14 +433,12 @@ class EmailActiveView(View):
         return redirect(reverse('users:center'))
 
 
-# 用户中心收货地址视图
+# 用户中心收货地址管理
 class AddressView(View):
 
     def get(self, request):
 
         return render(request, 'user_center_site.html')
-
-
 
 
 
