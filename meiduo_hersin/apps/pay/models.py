585acdeec8ad0af4da95cb42695236079c89c5c2
hersin <1508625083@qq.com>
2019-06-30 16:07:04 +0800

    生成订单和交易流水的模型

--- meiduo_hersin/apps/pay/models.py
+++ meiduo_hersin/apps/pay/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+# 我们需要将订单编号和交易流水号进行关联存储，方便用户和商家后续使用。
+
 # 定义支付结果模型类
 from django.db import models
 from apps.orders.models import OrderInfo
@@ -17,3 +19,4 @@ class Payment(BaseModel):
         db_table = 'tb_payment'
         verbose_name = '支付信息'
         verbose_name_plural = verbose_name
+
